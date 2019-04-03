d6f221dcf563cecc5c8a07b4716dbd825d40a9a0
marc-gade <marc.gade.marc@gmail.com>
2019-04-03 14:32:37 +0200

    refresh token

--- bin/ah_api/req.js
+++ bin/ah_api/req.js
@@ -3,17 +3,17 @@ var Api = require(appRoot + '/models/api')
 var logger = require(appRoot + '/app').logger
 var apiAuth = require(appRoot + '/bin/ah_api/auth')
 
-module.exports.apiRequest = function(api, path, callback) {
+module.exports.apiRequest = function (api, path, callback) {
   //Check if token is not expired
-  if (new Date(Api.expireAt).getTime() < new Date().getTime()) {
-    apiAuth.refreshToken(api.refreshToken, Api.getRedirectUrl(), Api.getSecret(), Api.getClientId(), function(apiDataString) {
+  if (new Date(api.expireAt).getTime() < new Date().getTime()) {
+    apiAuth.refreshToken(api.refreshToken, Api.getRedirectUrl(), Api.getSecret(), Api.getClientId(), function (apiDataString) {
       if (apiDataString) {
         var apiDataJSON = JSON.parse(apiDataString)
         if (apiDataJSON.hasOwnProperty('data')) {
           var apiReg = new Api.ApiSerializer(apiDataJSON.data)
           apiReg.id = api.id
           apiReg.SchoolId = api.SchoolId
-          apiReg.updateDB(function(err) {
+          apiReg.updateDB(function (err) {
             if (err) {
               Error.render(err)
             } else {
@@ -54,17 +54,17 @@ function sendApiRequest(api, path, callback) {
   }
   logger.info(options)
   result.request.options = options
-  var req = https.request(options, function(res) {
+  var req = https.request(options, function (res) {
     result.result.status = res.statusCode
     logger.info('STATUS: ' + result.result.status)
     result.result.headers = JSON.stringify(res.headers)
     logger.info('HEADERS: ' + result.result.headers)
     res.setEncoding('utf8')
     var data = ''
-    res.on('data', function(chunk) {
+    res.on('data', function (chunk) {
       data += chunk
     })
-    res.on('end', function() {
+    res.on('end', function () {
       if (data != '') {
         var dataJSON = JSON.parse(data)
         result.data = dataJSON.data
@@ -81,7 +81,7 @@ function sendApiRequest(api, path, callback) {
       }
     })
   })
-  req.on('error', function(err) {
+  req.on('error', function (err) {
     callback(err, null)
   })
 
