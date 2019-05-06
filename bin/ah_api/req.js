05cb10297a4a7b0ef6cbdd8cf9eb8a262e5b475b
marc-gade <marc@matierenoire.io>
2019-05-06 11:23:00 +0200

    Update expireat condition

--- bin/ah_api/req.js
+++ bin/ah_api/req.js
@@ -5,7 +5,7 @@ var apiAuth = require(appRoot + '/bin/ah_api/auth')
 
 module.exports.apiRequest = function (api, path, callback) {
   //Check if token is not expired
-  if (new Date(Api.expireAt).getTime() < new Date().getTime()) {
+  if (new Date(api.expireAt).getTime() < new Date().getTime()) {
     apiAuth.refreshToken(api.refreshToken, Api.getRedirectUrl(), Api.getSecret(), Api.getClientId(), function (apiDataString) {
       if (apiDataString) {
         var apiDataJSON = JSON.parse(apiDataString)
